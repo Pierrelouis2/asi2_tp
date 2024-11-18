8859ba492efb68254f52d48a4cac6a3d72efcd52
Pierre-louis <pierre-louis.telep@cpe.fr>
2024-11-18 23:59:34 +0100

    refactor message handling route and update package dependencies

--- asi2_tp/nodejs-app/index.mjs
+++ asi2_tp/nodejs-app/index.mjs
@@ -13,32 +13,31 @@ app.use(express.json());
 app.use(express.static(CONFIG.www))
 
 
-app.route('/msg')
-    .post((req,res) => {
-        console.log(req.body);
-        // Connexion à ActiveMQ et envoi du message
-        try {
-            stompit.connect(CONFIG.connectOptions, (error, client) => {
-                if (error) {
-                    console.error('Connection error: ' + error.message);
-                    res.status(500).send('Connection error');
-                    return;
-                }
+post('/msg',(req,res) => {
+    console.log(req.body);
+    // Connexion à ActiveMQ et envoi du message
+    try {
+        stompit.connect(CONFIG.connectOptions, (error, client) => {
+            if (error) {
+                console.error('Connection error: ' + error.message);
+                res.status(500).send('Connection error');
+                return;
+            }
 
-                const frame = client.send({
-                    'destination': '/queue/cpe-test',
-                });
-                frame.write(JSON.stringify(req.body));
-                frame.end();
-                client.disconnect();
-            })
-        ;}
-        catch (e) {
-            console.error('Error: ' + e.message);
-            res.status(500).send('Error');
-        }
-        io.emit('msg', req.body);
-    });
+            const frame = client.send({
+                'destination': '/queue/cpe-test',
+            });
+            frame.write(JSON.stringify(req.body));
+            frame.end();
+            client.disconnect();
+        })
+    ;}
+    catch (e) {
+        console.error('Error: ' + e.message);
+        res.status(500).send('Error');
+    }
+    io.emit('msg', req.body);
+});
 
 app.listen(CONFIG.port, () =>
     console.log(`Listening http://localhost:${CONFIG.port}`)
